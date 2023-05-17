f0cda4552da24895d2576eb0a71a13489d17c308
eurorondon03 <eurorondon03@gmail.com>
2023-05-17 10:58:11 -0400

    add git actions v5

--- src/components/Uploader/Uploader.tsx
+++ src/components/Uploader/Uploader.tsx
@@ -1,7 +1,7 @@
 import React, { ChangeEvent } from "react";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { uploadPhoto } from "../../api/photo";
-import { ArrowUpTrayIcon, BeakerIcon } from '@heroicons/react/24/solid'
+import { ArrowUpTrayIcon } from "@heroicons/react/24/solid";
 
 export const Uploader: React.FC = () => {
   const queryClient = useQueryClient();
@@ -21,9 +21,9 @@ export const Uploader: React.FC = () => {
   };
   return (
     <form>
-      <label  className=" flex justify-center flex-col  items-center border-dashed  border-2 border-slate-400 rounded-lg py-8 bg-white">
+      <label className=" flex justify-center flex-col  items-center border-dashed  border-2 border-slate-400 rounded-lg py-8 bg-white">
         <p> Upload files:</p>
-        <ArrowUpTrayIcon className="h-12 w-12 text-blue-500"/>
+        <ArrowUpTrayIcon className="h-12 w-12 text-blue-500" />
         <input type="file" multiple onChange={uploadFile} className="hidden" />
       </label>
     </form>
